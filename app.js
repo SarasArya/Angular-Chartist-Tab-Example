7f63d9a64d4521e4f341d42bd8395863fbb2dce5
SarasArya <saras.arya@gmail.com>
2016-03-17 17:58:11 +0530

    fixed the chartist.update issue

--- app.js
+++ app.js
@@ -1,72 +1,72 @@
 var app = angular.module('chartist', ['angular-chartist', 'ngMaterial', 'ngAnimate', 'ngAria']);
 
-app.controller('chartistController', function($scope, $timeout,ChartService) {
+app.controller('chartistController', function($scope, $timeout, ChartService, $document) {
     $scope.allTheCharts = ChartService.charts();
-    $scope.tabToggled = function(tab){
-            $timeout(function(){
-                angular.element(tab).find('chartist').each(function(i, e) {
+    $scope.tabToggled = function(tab) {
+       $timeout(function() {
+                angular.element('#'+tab).each(function(i, e) {
+                    console.log("Updating");
                     e.__chartist__.update();
                 });
-           
-             }, 100);
 
-        };
+            }, 100);
+    };
 });
 
 app.service('ChartService', function() {
     this.charts = function() {
-    	var charts = {
-    		TAB_1: {
-            data: {
-                labels: ["Mon", "Tue", "Wed", "Thur", "Fri"],
-                series: [
-                    [1, 10, 13, 5, 3]
-                ]
-            },
-            options: {
-                axisX: {
-                    showGrid: true,
+        var charts = {
+            TAB_1: {
+                data: {
+                    labels: ["Mon", "Tue", "Wed", "Thur", "Fri"],
+                    series: [
+                        [1, 10, 13, 5, 3]
+                    ]
                 },
-                axisY: {
-                    showGrid: true,
+                options: {
+                    axisX: {
+                        showGrid: true,
+                    },
+                    axisY: {
+                        showGrid: true,
+                    }
                 }
-            }
-        },
+            },
 
-        TAB_2: {
-            data: {
-                labels: ["Mon", "Tue", "Wed", "Thur", "Fri"],
-                series: [
-                    [20, 10, 30, 40, 80]
-                ]
-            },
-            options: {
-                axisX: {
-                    showGrid: true,
+            TAB_2: {
+                data: {
+                    labels: ["Mon", "Tue", "Wed", "Thur", "Fri"],
+                    series: [
+                        [20, 10, 30, 40, 80]
+                    ]
                 },
-                axisY: {
-                    showGrid: true,
+                options: {
+                    axisX: {
+                        showGrid: true,
+                    },
+                    axisY: {
+                        showGrid: true,
+                    }
+                }
+            },
+            TAB_3: {
+                data: {
+                    labels: ["Mon", "Tue", "Wed", "Thur", "Fri"],
+                    series: [
+                        [1, 10, 34, 5, 3]
+                    ]
+                },
+                options: {
+                    axisX: {
+                        showGrid: true,
+                    },
+                    axisY: {
+                        showGrid: true,
+                    }
                 }
             }
-        },
-        TAB_3: {
-            data: {
-                labels: ["Mon", "Tue", "Wed", "Thur", "Fri"],
-                series: [
-                    [1, 10, 34, 5, 3]
-                ]
-            },
-            options: {
-                axisX: {
-                    showGrid: true,
-                },
-                axisY: {
-                    showGrid: true,
-                }
-            }
-        }
-    	};
-    	return charts;
+        };
+        return charts;
     };
-    
+
 });
